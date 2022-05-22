01afaed0040197352fa267c98bb9737d862f86f4
Najmul890 <najmul890890@gmail.com>
2022-05-22 00:12:07 -0700

    connected with mongodb done

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const ObjectId = require('mongodb').ObjectId;
 require('dotenv').config();
 
 const app = express();
@@ -8,11 +10,31 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.pukwa.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+async function run() {
+    try {
+         await client.connect();
+         const wheelCollection = client.db('wheelManufacture').collection('wheels');
+
+         
+
+    }
+    finally {
+
+    }
+}
+
+run().catch(console.dir);
+
 
 app.get('/', (req, res) => {
-    res.send('Server is running')
+    res.send('Wheel Manufacturing LTD server is running')
 })
 
 app.listen(port, () => {
     console.log(`Server is running successfully ${port}`)
 })
+
